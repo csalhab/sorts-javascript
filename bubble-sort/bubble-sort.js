7520c73f8a04562598fb19f093ac6de9c9180daa
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-10 17:08:30 -0500

    created bubble-sort.js file and added pseudo code

---
+++ bubble-sort/bubble-sort.js
@@ -0,0 +1,21 @@
+// Create our input data
+
+// seed data in unsortedInputArray
+// for (let i = 0; i < 2000; i++) {
+//   unsortedInputArray.push(Math.round(Math.random() * 2000));
+// }
+
+// TODO: create bubble sort function
+
+//bubble sort compares adjacent values
+//if item that's before is greater than the item that follows it
+//then swap the items
+//in order to swap, must first store item that's before temporarily
+//must do all this in a loop
+
+//must perhaps wrap loop inside another loop to do entire array several times
+
+//return array when swapping done
+
+// const sorted = bubbleSort(unsortedInputArray);
+//good example to show how just for loop is not enough
