91ea0efb7e601aa332a57fa8baadf0707f6cba68
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-10 17:32:52 -0500

    created bubbleSort array function and console logs to get bearings

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -1,24 +1,33 @@
-// Create our input data
-const unsortedInputArray = [];
+// Create array that will hold unsorted values
+const unsortedArray = [];
 
 // seed data in unsortedInputArray
 for (let i = 0; i < 100; i++) {
-  unsortedInputArray.push(Math.round(Math.random() * 100));
+  unsortedArray.push(Math.round(Math.random() * 100));
 }
 
-console.log("unsortedInputArray: " + unsortedInputArray);
+console.log("unsortedArray values: " + unsortedArray);
 
-// TODO: create bubble sort function
+// TODO: create bubble sort array function
+const bubbleSort = (array) => {
+  console.log("inside bubbleSort array function, array received: " + array);
 
-//bubble sort compares adjacent values
-//if item that's before is greater than the item that follows it
-//then swap the items
-//in order to swap, must first store item that's before temporarily
-//must do all this in a loop
+  //bubble sort compares adjacent values
+  //if item that's before is greater than the item that follows it
+  //then swap the items
+  //in order to swap, must first store item that's before temporarily
+  //must do all this in a loop
 
-//must perhaps wrap loop inside another loop to do entire array several times
+  //must perhaps wrap loop inside another loop to do entire array several times
 
-//return array when swapping done
+  //return array when swapping done
+  return array;
+};
 
-// const sorted = bubbleSort(unsortedInputArray);
+// const sorted = bubbleSort(unsortedArray);
 //good example to show how just for loop is not enough
+const myArray = [3, 2, 1];
+console.log("Pre Sort Array:", myArray.join(" "));
+const sorted = bubbleSort(myArray);
+console.log("Post Sort Array:", sorted);
+console.log("DONE!");
