16584a2f9feb31874eb9ca466963db4420ae2262
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-13 01:52:59 -0500

    created merge-sort folder and js file and added pseudo code

---
+++ merge-sort/merge-sort.js
@@ -0,0 +1,7 @@
+// Create our unsorted array
+
+// seed data into the unsorted array
+
+//split unsorte array until ending up with multiple arrays that only contain one element
+
+// TODO: create merge sort function
