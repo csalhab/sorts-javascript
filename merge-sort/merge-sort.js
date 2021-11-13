91a2a49d21939cdc323a5c021001cb95d3d3c796
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-13 03:18:13 -0500

    coded mergeArrays function

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -10,20 +10,30 @@ for (let i = 0; i < 15; i++) {
 const mergeArrays = (leftArray, rightArray) => {
   //create new array that our sorted array values from left and right will go into
   const resultArray = [];
+
+  //keep track of what index we're at in each array for the following loop
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  //as long as there is at least one value left in both arrays, loop
+  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+    //if the value of the leftArray at its current index is less than the value of the rightArray at its current index, that means the leftArray value should be pushed into the resultArray
+    if (leftArray[leftIndex] < rightArray[rightIndex]) {
+      resultArray.push(leftArray[leftIndex]);
+      // Now that we've sorted the leftArray at that index, let's move onto the next one
+      leftIndex++;
+    } else {
+      //else if the rightArray value is less than the leftArray value, push that right value into the resultArray instead and move onto the next index of the rightArray
+      resultArray.push(rightArray[rightIndex]);
+      rightIndex++;
+    }
+  }
+  // the resultArray may not be sorted just yet, but it's closer to being sorted so the splitArray() function can split it and sort it again
+  // since the while loop may not have gotten to every index of the leftArray or rightArray arrays, we concatenate the leftover values back into resultArray
+  return resultArray
+    .concat(leftArray.slice(leftIndex))
+    .concat(rightIndex.slice(rightIndex));
 };
-
-//keep track of what index we're at in each array for the following loop
-
-//as long as there is at least one value left in both arrays, loop
-
-//if the value of the leftArray at its current index is less than the value of the rightArray at its current index, that means the leftArray value should be pushed into the resultArray
-
-// Now that we've sorted the leftArray at that index, let's move onto the next one
-
-//else if the rightArray value is less than the leftArray value, push that value into the resultArray instead and move onto the next index of the rightArray
-
-// the resultArray may not be sorted just yet, but it's closer to being sorted so the splitArray() function can split it and sort it again
-// since the while loop may not have gotten to every index of the leftArray or rightArray arrays, we concatenate the leftover values back into resultArray
 
 // TODO: create splitArray() function
 //split unsorted array until ending up with multiple arrays that only contain one element
