f60a3292880b6a738ece9a22210f12a761ed7f2d
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-15 04:27:06 -0500

    added selection sort folder and file with puesdo code

---
+++ selection-sort/selection-sort.js
@@ -0,0 +1,17 @@
+// Create our unsorted array
+
+// seed data in unsortedArray
+
+//TODO: create selectionSort() function
+//start by making the 1st element as the smallest number
+//then loop thru to find the actual smallest number in array by checking if next element is smaller than starting 1st element
+
+//check if smallest is actually the 1st element
+//if it is nothing else needed
+//but if it's not, swap the elements
+
+//return sorted array
+
+//INITIALIZATION - kick it off
+
+//ECHO OUT - log out values
