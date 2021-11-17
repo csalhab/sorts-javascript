30cb8613ba064232d791ff2241d8ecdb7261929a
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-16 19:48:39 -0500

    created insertion-sort folder and file with pseudocode

---
+++ insertion-sort/insertion-sort.js
@@ -0,0 +1,10 @@
+// Create our unsorted array
+
+// seed data, random integers in the range of 0 to 15, into the unsorted array
+
+// TODO: create mergeArrays() function to merge sorted values
+
+//INITIALIZATION - kick it off
+
+//ECHO OUT - log out values
+console.log("Post Sort: ");
