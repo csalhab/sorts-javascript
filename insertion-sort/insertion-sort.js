87a8e031dcec370b85713f9e5f7bfad4a9b03666
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-11-16 20:26:22 -0500

    added logging to help see walk thru results

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -3,31 +3,29 @@ const unsortedArray = [];
 
 // seed data, generate 15 random integers in the range of 0 to 50, into the unsorted array
 for (let i = 0; i < 15; i++) {
-  unsortedArray.push(Math.round(Math.random * 50));
+  unsortedArray.push(Math.round(Math.random() * 50));
 }
+
+console.log("PRE unsorted: " + unsortedArray);
 
 // TODO: create insertionSort() function
 function insertionSort(inputArray) {
-  // sort an array of size n in ascending order
-  // start by iterating from unsortedArray[1] to unsortedArray[n]
-  // compare the current element (key) to its predecessor
-  // if the key element is smaller than its predecessor, compare it to the elements before
-  // move the greater elements one position up to make space for the swapped element
-  let n = inputArray.length;
-  for (let i = 1; i < n; i++) {
-    // Choosing the first element in our unsorted subarray
+  for (let i = 1; i < inputArray.length; i++) {
+    // set current to be the first element in our unsortedArray
     let current = inputArray[i];
-    // The last element of our sorted subarray
+    // set j to hold the previous element of our sortedArray
     let j = i - 1;
     while (j > -1 && current < inputArray[j]) {
       inputArray[j + 1] = inputArray[j];
       j--;
     }
     inputArray[j + 1] = current;
+    console.log("insertionSort: " + inputArray);
   }
   return inputArray;
 }
 //INITIALIZATION - kick it off
+const sortedArray = insertionSort(unsortedArray);
 
 //ECHO OUT - log out values
-console.log("Post Sort: ");
+console.log("Post Sort: ", sortedArray.join(" "));
